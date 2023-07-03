8b031bfa1c8f8109312b3a67c79e2f21c93aa8e9
arpitanaskar <arpitanaskarphone@gmail.com>
2023-07-03 13:00:38 +0530

    ObjectLocalStorage

--- script.js
+++ script.js
@@ -2,23 +2,46 @@ let submit = document.getElementById('getcall');
 submit.addEventListener('click', storeValues);
 
 
+// function storeValues() {
+//     let name = document.getElementById('name').value.toString();
+//     localStorage.setItem('Name', name)
+
+//     let surname = document.getElementById('surname').value.toString();
+//     localStorage.setItem('Surname', surname)
+
+//     let email = document.getElementById('email').value.toString();
+//     localStorage.setItem('Email', email)
+
+//     let number = document.getElementById('number').value.toString();
+//     localStorage.setItem('Phone No.', number)
+
+//     let date = document.getElementById('date').value.toString();
+//     localStorage.setItem('Date', date)
+
+//     let time = document.getElementById('timeInput').value.toString();
+//     localStorage.setItem('Time for call', time)
+
+// }
+
+
+// let name = document.getElementById('name').value.toString();
+// let surname = document.getElementById('surname').value.toString();
+// let email = document.getElementById('email').value.toString();
+// let number = document.getElementById('number').value.toString();
+// let date = document.getElementById('date').value.toString();
+// let time = document.getElementById('timeInput').value.toString()
+
+
 function storeValues() {
-    let name = document.getElementById('name').value.toString();
-    localStorage.setItem('Name', name)
+    let obj = {
+        name: document.getElementById('name').value,
+        surname: document.getElementById('surname').value,
+        email: document.getElementById('email').value,
+        number: document.getElementById('number').value,
+        date: document.getElementById('date').value,
+        time: document.getElementById('timeInput').value
+    }
 
-    let surname = document.getElementById('surname').value.toString();
-    localStorage.setItem('Surname', surname)
-
-    let email = document.getElementById('email').value.toString();
-    localStorage.setItem('Email', email)
-
-    let number = document.getElementById('number').value.toString();
-    localStorage.setItem('Phone No.', number)
-
-    let date = document.getElementById('date').value.toString();
-    localStorage.setItem('Date', date)
-
-    let time = document.getElementById('timeInput').value.toString();
-    localStorage.setItem('Time for call', time)
-    
+    let obj_serialized = JSON.stringify(obj);
+    localStorage.setItem(document.getElementById('email').value.toString(), obj_serialized);
 }
